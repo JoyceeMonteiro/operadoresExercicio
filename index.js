ff15a65e3cafee91f394ad5e46c2d33b824a6493
JoyceeMonteiro <contato.joymonteiro@gmail.com>
2023-04-27 08:54:57 -0300

    Subindo exercicio de operadores.

---
+++ index.js
@@ -0,0 +1,102 @@
+// console.log("hello")
+// //comentario
+// //prompt("Digite seu nome")
+// const nome = prompt("Digite seu nome")
+// const idade = prompt("Digite sua idade")
+// const endereco = prompt("Digite seu endereço.")
+// console.log(nome)
+// console.log(idade)
+// console.log(endereco)
+
+
+//let anoNascimento = 1997
+//anoNascimento = 2023 - anoNascimento
+// console.log(anoNascimento)
+
+//  const nome = prompt("Digite seu nome")
+//  const idade = prompt("Digite sua idade")
+
+//  console.log("Olá!"   "Meu nome é" , nome, "e eu tenho " , idade, "anos.")
+
+// const nome = prompt("Digite seu nome:")
+// const sobrenome = prompt("Digite seu sobrenome:")
+// const idade = prompt("Digite sua idade:")
+// const estudante = prompt("Você é estudante? (Sim / Não) ?")
+
+// console.log("Nome:" , nome, sobrenome, "Idade:" , idade, "Estudante:" , estudante) 
+// 26/04
+//  const got = "Game of Thrones"
+//  const temporadas = 8
+
+// //  typeof got // String
+// //  typeof temporadas// number
+
+//  console.log(typeof got)
+//  console.log (typeof temporadas)
+
+// let idade = Number(prompt(" Digite sua idade"))
+// console.log (typeof idade)
+
+
+//interpretção 26/04
+
+
+//*******ANÁLISE 1
+// let a = 10 // atribuindo valor ao A
+// let b = 10 // Atribuindo valor ao B
+
+// console.log(b) // imprime o valor atribuido a B
+
+// b = 5 // atribui um novo valor ao B
+// console.log(a, b) // imprime o A com o novo valor de B que esta dentro deste novo console.
+
+//******ANÁLISE 2
+// let a = 10 // atribui um valor  ao A
+// let b = 20// atribui um valor ao B
+// c = a //imprime no C o valor atribuido ao A
+// b = c // Imprime no B o valor de c(que é o mesmo de A)
+// a = b  // Imprime no A o valor atribuido ao B 
+
+// console.log(a, b, c) //Exibe os valores acima 
+
+// //**** ANÁLISE 3
+// let horasPorDia = prompt("Quantas horas você trabalha por dia?") // mudança da variavel p para horasPorDia
+// let salarioPorDia = prompt("Quanto você recebe por dia?") //mudança da variável t para SalarioPorDia
+// alert(`Voce recebe ${salarioPorDia/horasPorDia} por hora`)// mudança de t/p para salarioPorDia/HorasPorDia
+
+//**** EXERCÍCIO 1
+// let nome
+// let idade
+
+// nome = prompt("Digite seu nome:")
+// idade = Number(prompt(" Digite sua idade:"))
+
+// console.log(typeof nome)
+// // console.log (typeof idade)
+// console.log("Olá," , nome, "você tem" , idade, "anos")
+
+
+//*****EXERCÍCIO 2
+
+// let cidade = prompt("Você mora em São Leopoldo? (Sim / Não) ?")
+// let bairro = prompt("Você considera seu bairro seguro? (Sim / Não) ?")
+// let morar = prompt("Você gosta de morar no bairro? (Sim / Não) ?")
+
+// let local = cidade
+// let endereco = bairro
+// let reside = morar
+
+// console.log("Você mora em São Leopoldo?" , local, "Você considera seu bairro seguro?" , endereco, "Você gosta de morar no bairro?" , reside)
+
+//****EXERCICIO 3
+ let a = Number(prompt("Digite um número"))
+ let b = Number(prompt("Digite outro número"))
+
+let c = b
+b = a
+a = c
+
+console.log("Valor do primeiro número" ,a)
+console.log("Valor do outro número" ,b)
+
+
